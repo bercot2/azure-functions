ef3f9764e6fa46394cdc208219811942104cc33c
Bruno Mendes <brunobercot2012@gmail.com>
2025-01-12 06:04:55 -0300

    removendo if que não usava

--- function_app.py
+++ function_app.py
@@ -38,9 +38,6 @@ import logging
     schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False
 )
 def timer_trigger(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
-        logging.info("The timer is past due!")
-
     logging.info("Python timer trigger function executing.")
 
     # Executa a lógica assíncrona para processar as requisições
